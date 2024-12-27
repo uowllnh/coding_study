a328c893aed2caa315620c728b266a6845ce23f3
hyunjune <hun2.ch98@gmail.com>
2024-12-27 22:56:16 +0900

    Create hyunjune.py

---
+++ Baekjoon/문제풀이/에디터/hyunjune.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.read
+data = input().strip().split('\n')
+init_string = data[0]
+commands = data[2:]  
+
+left_stack = list(init_string)
+right_stack = []
+
+for command in commands:
+    if command[0] == 'L' and left_stack:
+        right_stack.append(left_stack.pop())
+    elif command[0] == 'D' and right_stack:
+        left_stack.append(right_stack.pop())
+    elif command[0] == 'B' and left_stack:
+        left_stack.pop()
+    elif command[0] == 'P':
+        left_stack.append(command[2])
+
+print(''.join(left_stack + right_stack[::-1]))
