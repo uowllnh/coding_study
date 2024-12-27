c8d65cc5cf7a632bd05a168eae6a4d9416a05e49
hyunjune <hun2.ch98@gmail.com>
2024-12-27 23:25:21 +0900

    Create hyunjune.java

---
+++ Baekjoon/문제풀이/큐/hyunjune.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Queue;
+
+public class baek10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
