cc2674bcc8cfe64dc798e246f88ab8005fa232e4
hyunjune <hun2.ch98@gmail.com>
2024-12-28 10:59:47 +0900

    Create 요세푸스 문제 hyunjune.java

---
+++ Baekjoon/문제풀이/요세푸스 문제/hyunjune.java
@@ -0,0 +1,14 @@
+// 자바코드에서 리스트를 만들거나 큐 구조체를 만드는 방법과 해당 구조에서의 함수 미흡...
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class baek1158 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        
+        scanner.close();
+    }
+}
