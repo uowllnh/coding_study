c9d32d95bb8608e5c3475794f672ccac03232fb7
yoooory <uowllnh@gmail.com>
2025-01-05 12:47:42 +0900

    Create 단어 뒤집기2 yuri.py

---
+++ Baekjoon/문제풀이/단어 뒤집기2/yuri.py
@@ -0,0 +1,28 @@
+s = input().strip()
+result = []
+temp = []
+inside_tag = False
+
+for c in s:
+    if c == '<':
+        if temp:
+            result.append(''.join(temp[::-1]))
+            temp = []
+        inside_tag = True
+        result.append(c)
+    elif c == '>':
+        inside_tag = False
+        result.append(c)
+    elif inside_tag:
+        result.append(c)
+    elif c == ' ':
+        result.append(''.join(temp[::-1]))
+        result.append(c)
+        temp = []
+    else:
+        temp.append(c)
+
+if temp:
+    result.append(''.join(temp[::-1]))
+
+print(''.join(result))
