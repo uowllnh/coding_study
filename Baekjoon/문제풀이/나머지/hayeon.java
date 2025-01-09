02853f76942dff6618dbee1e5810e15629dece86
uo2hy2 <uo2h.y2@gmail.com>
2025-01-09 22:38:11 +0900

    Update hayeon.java

--- Baekjoon/문제풀이/나머지/hayeon.java
+++ Baekjoon/문제풀이/나머지/hayeon.java
@@ -7,19 +7,17 @@ import java.io.InputStreamReader;
 public class hayeon {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         String[] input = br.readLine().split(" ");
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
         int c = Integer.parseInt(input[2]);
-        
-        double result1 = (a + b) % (double)c;
-        double result2 = ((a%(double)c) + (b%(double)c)) % c;
-        double result3 = (a * b) % (double)c;
-        double result4 = ((a % (double)c) * (b % (double)c)) % c;
-        
-        System.out.println(result1 + " " + result2 + " " + result3 + " " + result4);
-        
+
+        System.out.println((a + b) % c);
+        System.out.println(((a%c) + (b%c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+
         br.close();
     }
 }
