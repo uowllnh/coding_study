ca588557a8200a144675d8568c43655fdcafe4e9
jeongeon <kimjeongeon97@gmail.com>
2025-02-08 16:25:55 +0900

    Create 1로 만들기 jeoeo

--- Baekjoon/문제풀이/1로 만들기/jeoeo.py
+++ Baekjoon/문제풀이/1로 만들기/jeoeo.py
@@ -1,14 +1,15 @@
-N = int(input())
+import sys
 
-a = [0] * 10000001
+n = int(sys.stdin.readline().strip())
+a = [0] * (n + 1)
 
-for i in range (2, N+1) :
-    a[i] = a[i-1] + 1
+for i in range(2, n + 1):
+    a[i] = a[i - 1] + 1 
     
     if i % 2 == 0:
-        a[i] = min(a[i], a[i//2] + 1) 
+        a[i] = min(a[i], a[i // 2] + 1)
     
-    if i % 3 == 0 :
-        a[i] = min(a[i], a[i//3] + 1)
+    if i % 3 == 0:
+        a[i] = min(a[i], a[i // 3] + 1)
 
-print(a[N])
+print(a[n])
