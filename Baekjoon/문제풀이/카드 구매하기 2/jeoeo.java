3b2fd47c32bb5fd0607e93680a3979c150e923ac
jeongeon <kimjeongeon97@gmail.com>
2025-02-09 00:58:15 +0900

    Create 카드구매2 jeoeo.java

---
+++ Baekjoon/문제풀이/카드 구매하기 2/jeoeo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class jeoeo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] p = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            p[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + p[j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+        sc.close();
+    }
+}
