d6ffb2f7148415c9497469bffa4577bec91fe3ab
jeongeonK <120458630+jeongeonK@users.noreply.github.com>
2025-02-09 13:56:05 +0900

    Update jeoeo.py

--- Baekjoon/문제풀이/카드 구매하기 2/jeoeo.py
+++ Baekjoon/문제풀이/카드 구매하기 2/jeoeo.py
@@ -3,12 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 p = [0] + list(map(int, input().split()))
-dp = [False for _ in range(n+1)]
+dp = [float('inf')] * (n+1)  
+dp[0] = 0
 
 for i in range(1, n+1):
     for j in range(1, i+1): 
-        if dp[i] == False:
-            dp[i] = dp[i-j] + p[j]
-        else:
-            dp[i] = min(dp[i], dp[i-j] + p[j])
+        dp[i] = min(dp[i], dp[i-j] + p[j])
+        
 print(dp[n])
