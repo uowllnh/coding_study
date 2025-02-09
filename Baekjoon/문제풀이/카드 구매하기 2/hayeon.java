c9ada480ebc48b7f299a3774e01b201b0ca6ed72
uo2hy2 <uo2h.y2@gmail.com>
2025-02-09 12:38:21 +0900

    Create 카드 구매하기 2 hayeon.java

---
+++ Baekjoon/문제풀이/카드 구매하기 2/hayeon.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class hayeon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int[] p = new int[n + 1];   
+        for (int i = 1; i <= n; i++) {
+            p[i] = scanner.nextInt(); 
+        }
+        
+        int[] dp = new int[n + 1];  
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;  
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + p[j]);
+            }
+        }
+        
+        System.out.println(dp[n]);  
+        scanner.close();
+    }
+}
